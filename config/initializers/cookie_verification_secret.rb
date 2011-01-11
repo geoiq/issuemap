b5e56f4954c11972a7cab04c5d91bcd026da3a14
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-11 16:42:37 -0500

    Removed unused (and deprecated) cookie_verifier_secret

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '6b700f1094868f03fd6a5a0e5484df2404c18fd71fa8db0a5acca083156d14106653fcd3be17a211dcf9466c5c33800708fe563d642b751afb1eae0725db4a2b';
