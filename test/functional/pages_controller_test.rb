dcf23e6b0e6771b6394f3f7ec537db2627c553d6
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-13 17:02:03 -0500

    Added test for static content under the pages controller [#4553] [#4555]

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  def self.should_get_page(page, path = "/pages/#{page}", template = page)
+    should route(:get, path).to(:action => page, :controller => "pages")
+    context "A page request for #{page}" do
+      setup { get page }
+      should respond_with :success
+      should render_template template
+    end
+  end
+
+  should "route the root path to the home action" do
+    assert_recognizes({ :controller => "pages", :action => "home" }, "/")
+  end
+
+  should_get_page :home
+  should_get_page :privacy
+  should_get_page :tos
+  should_get_page :faq
+  should_get_page :usage
+end
