3c22b58ffbcc47befe7df13ed4c6ec9e7136c3ab
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-14 21:28:07 -0500

    New layout and styles for home page and content pages [#4554][#4553][#4555]
    * Updated to compass and sass styling
    * Refactored to use haml in the layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_flash_messages(*keys)
+    messages = keys.map do |key|
+      content_tag(:p, flash_message_with_item(key), :class => "flash #{key}") if flash[key]
+    end.join
+    content_tag(:div, messages, :id => "flash_messages") unless messages.blank?
+  end
+
+  def flash_message_with_item(key)
+    item = flash["#{key}_item".to_sym]
+    substitution = item.is_a?(Array) ? link_to(*item) : item
+    flash[key] % substitution
+  end
 end
