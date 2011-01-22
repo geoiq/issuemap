b58e809cdcc29586c4ccea2e73da956da992019b
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-21 19:04:40 -0500

    Added Open Graph support for better sharing on Facebook [Finishes #4887]

---
+++ app/helpers/open_graph_helper.rb
@@ -0,0 +1,55 @@
+module OpenGraphHelper
+  def og_title(string)
+    content_for(:og_title, string)
+  end
+
+  def og_title_tag(default = nil)
+    content = @content_for_og_title || default
+    tag(:meta, :name => "og:title", :content => content) unless content.blank?
+  end
+
+  def og_type(string)
+    content_for(:og_type, string)
+  end
+
+  def og_type_tag(default = nil)
+    content = @content_for_og_type || default
+    tag(:meta, :name => "og:type", :content => content) unless content.blank?
+  end
+
+  def og_url(string)
+    content_for(:og_url, string)
+  end
+
+  def og_url_tag(default = nil)
+    content = @content_for_og_url || default
+    tag(:meta, :name => "og:url", :content => content) unless content.blank?
+  end
+
+  def og_image(string)
+    content_for(:og_image, string)
+  end
+
+  def og_image_tag(default = nil)
+    content = @content_for_og_image || default
+    tag(:meta, :name => "og:image", :content => content) unless content.blank?
+  end
+
+  def og_site_name(string)
+    content_for(:og_site_name, string)
+  end
+
+  def og_site_name_tag(default = nil)
+    content = @content_for_og_site_name || default
+    tag(:meta, :name => "og:site_name", :content => content) unless content.blank?
+  end
+
+  def og_description(string)
+    content_for(:og_description, string)
+  end
+
+  def og_description_tag(default = nil)
+    content = @content_for_og_description || default
+    tag(:meta, :name => "og:description", :content => content) unless content.blank?
+  end
+end
