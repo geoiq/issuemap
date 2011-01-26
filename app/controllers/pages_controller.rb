a9013ddb1c410e6b5efd4dd2cd5c2aeedf349abd
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-25 22:52:57 -0500

    Added config variable to manage featured maps in the home page gallery [Finishes #4554]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   caches_all_pages
 
   def home
-    @maps = Map.find(:all, :limit => 5)
+    map_tokens = AppConfig[:featured_map_tokens]
+    @maps = Map.find(:all, :conditions => { :token => map_tokens })
+    if @maps.size < 5
+      @maps.push(*Map.find(:all, :limit => (5 - @maps.size)))
+    end
   end
 end
