dab82dc200be6fd07008e0ce17a132b37dac7be5
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-27 16:22:03 -0500

    Updated and simplified capistrano's deploy.rb for deployments
    * Right now, only a qa environment is configured
    * Example usage:
        $ bundle exec cap qa deploy:long

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,71 +1,58 @@
-load_paths << File.join(File.dirname(__FILE__), 'deploy', 'tasks')
+require "bundler/capistrano"
 
-require 'capistrano/ext/multistage'
+set :application,  "issuemap"
+set :scm,          "git"
+set :repository,   "git@github.com:fortiusone/issuemap.git"
+set :git_enable_submodules, 1
+set :deploy_via,   :remote_cache
+set :use_sudo,     false
+set :runner,       "issuemap"
+set :admin_runner, "issuemap"
+set :ssh_options,  { :forward_agent => true }
 
-set :application, "issuemapper"
-set :stages, %(staging production qa)
-set :repository,  "git@github.com:fortiusone/issuemap.git"
-set :scm, :git 
-set :use_sudo, true
-set :user,          "root"
-set :deploy_user,   "root"
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-
-# If you have previously been relying upon the code to start, stop 
-# and restart your mongrel application, or if you rely on the database
-# migration code, please uncomment the lines you require below
-
-# If you are deploying a rails app you probably need these:
-
-#load 'ext/rails-database-migrations.rb'
-#load 'ext/rails-shared-directories.rb'
-
-# There are also new utility libaries shipped with the core these 
-# include the following, please see individual files for more
-# documentation, or run `cap -vT` with the following lines commented
-# out to see what they make available.
-
-# load 'ext/spinner.rb'              # Designed for use with script/spin
-# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
-# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
-
-set :deploy_to, "/fortiusone/live/apps/issuemapper"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-# see a full list by running "gem contents capistrano | grep 'scm/'"
-
-set :appserver, :passenger
-set :deploy_via, :remote_cache
-set :user, "root"
-
-desc "Re-establish symlinks"
-task :symlink do
-  run <<-CMD
-    ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml &&
-    ln -nfs #{shared_path}/config/config.yml   #{release_path}/config/config.yml
-  CMD
+task :qa do
+  set :rails_env,   "qa"
+  set :branch,      "master"
+  set :deploy_to,   "/fortiusone/live/apps/#{application}"
+  server "captest@issue-test.geoiq.com", :web, :app, :db, :primary => true
 end
 
-desc "Re-establish staging database"
-task :symlink_db do
-  run <<-CMD
-    ln -nfs #{shared_path}/staging.sqlite3 #{release_path}/db/staging.sqlite3
-  CMD
-end
+after "deploy:symlink", "deploy:symlink_configs"
+after "deploy:symlink", "deploy:update_stylesheets"
+after "deploy:restart", "deploy:cleanup"
 
-namespace :deploy do 
-  task :restart do 
-    run "touch #{current_path}/tmp/restart.txt"
+namespace :deploy do
+  desc "Long deploy will throw up the maintenance.html page and run migrations then it restarts and enables the site again."
+  task :long do
+    transaction do
+      update_code
+      web.disable
+      symlink
+      migrate
+    end
+
+    restart
+    web.enable
   end
-end
 
-namespace :tail do 
-  desc "View the server log"
-  task :log, :roles => :app do
-    # stream "tail -f #{shared_path}/log/production.log{,.0,.1,.2,.3,.4,.5,.6,.7}"
-    stream "tail -f #{current_path}/log/production.log"
+  task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
+    run <<-CMD
+      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml &&
+      ln -nfs #{shared_path}/config/config.yml   #{release_path}/config/config.yml
+    CMD
+  end
+
+  task :update_stylesheets, :roles => :app do
+    run "#{current_path}/script/runner -e #{rails_env} \"Sass::Plugin.options = { :always_update => true }; Sass::Plugin.update_stylesheets;\""
+  end
+
+  desc "Tail the Rails log for this environment"
+  task :logs, :roles => :app do
+    stream "tail -f #{shared_path}/log/#{rails_env}.log"
+  end
+
+  desc "Restart the app server."
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
--- config/deploy/staging.rb
+++
@@ -1,5 +0,0 @@
-role :web, "ec2-72-44-48-158.compute-1.amazonaws.com"
-role :app, "ec2-72-44-48-158.compute-1.amazonaws.com"
-role :db, "ec2-72-44-48-158.compute-1.amazonaws.com"
-
-after "deploy:update_code", "symlink", "permissions", "symlink_db"
